b7e6f3a4a2a1fa270607c7e76895718d7051f8e9
raflisalam <raflisalam.dokumen@gmail.com>
2023-09-23 12:32:15 +0800

    Update: gradle into 7.5 from 7.1.3

--- favorite/src/main/java/com/raflisalam/moviesapp/favorite/viewmodel/ViewModelFactory.kt
+++ favorite/src/main/java/com/raflisalam/moviesapp/favorite/viewmodel/ViewModelFactory.kt
@@ -9,7 +9,6 @@ class ViewModelFactory @Inject constructor(
     private val favoriteUseCase: FavoriteMoviesUseCase
 ) : ViewModelProvider.NewInstanceFactory() {
 
-    @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T =
         when {
             modelClass.isAssignableFrom(FavoriteViewModel::class.java) -> {
