bf721031a718928a8a568d8c83c87a846e93c220
raflisalam <raflisalam.dokumen@gmail.com>
2023-09-23 13:53:11 +0800

    Fix: issues on constants.kt/auth_key

--- core/src/main/java/com/raflisalam/moviesapp/core/common/constant/Constants.kt
+++ core/src/main/java/com/raflisalam/moviesapp/core/common/constant/Constants.kt
@@ -1,9 +1,7 @@
 package com.raflisalam.moviesapp.core.common.constant
 
-import com.raflisalam.moviesapp.core.BuildConfig
-
 object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
-    const val AUTH_KEY = BuildConfig.AUTH_KEY
+    const val AUTH_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjM5NzJkODVlZTBiMWZkZmQ4NmIzMjk2OGI4ZWZjYyIsInN1YiI6IjYwYTFjYTRjODM5MDE4MDA2ZDUwYWY3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B_GS-tGcVs5KPT4rECbqSTYoNSXtWQibz7xXCj6Jclw"
     const val path_image_base_url = "https://image.tmdb.org/t/p/w500"
 }
